086491ccfb237b80548c404ebf745ef93a9c9e3e
tania.prado97 <tania.prado97@ciencias.unam.mx>
2026-09-07 15:35:47 -0600

    names

--- practica03/Interp.hs
+++ practica03/Interp.hs
@@ -5,23 +5,23 @@ import Grammars
 -- RETO 3: sustitucion nominal que evita captura
 freeVars :: ASA -> [String]
 
-freeVars Num _ = []
-freeVars Boolean _ = []
-freeVars Id x = [x]
-freeVars Add e1 e2 = freeVars e1 ++ freeVars e2
-freeVars Sub e1 e2 = freeVars e1 ++ freeVars e2 
-freeVars Not e1 = freeVars e1
-freeVars Let x e1 e2 = freeVars e1 ++ (freeVars e2 filter (\= x))   
+freeVars (Num _) = []
+freeVars (Boolean _) = []
+freeVars (Id x )= [x]
+freeVars (Add e1 e2) = freeVars e1 ++ freeVars e2
+freeVars (Sub e1 e2) = freeVars e1 ++ freeVars e2 
+freeVars (Not e1) = freeVars e1
+freeVars (Let x e1 e2) = freeVars e1 ++ filter (/= x) freeVars e2
 
 names :: ASA -> [String]
 
-names Num _ = []
-names Boolean _ = []
-names Id x = [x]
-names Add e1 e2 = names e1 ++ names e2
-names Sub e1 e2 = names e1 ++ names e2
-names Not e1 = names e1
-names Let x e1 e2 = x : names e1 + names e2
+names (Num _) = []
+names (Boolean _)= []
+names (Id x) = [x]
+names (Add e1 e2) = names e1 ++ names e2
+names (Sub e1 e2) = names e1 ++ names e2
+names (Not e1) = names e1
+names (Let x e1 e2) = x : names e1 + names e2
 
 
 freshName :: [String] -> String
